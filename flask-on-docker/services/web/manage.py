218ab05beb5702e540eff33c0e8c4a69c2402798
Yuquimura <brianzambellitello@gmail.com>
2021-05-01 20:13:54 -0300

    Cambios varios * Reubicacion de config.py * Eliminacion de copias de archivos innecesarias * Implementacion de funcion create_db() en manage.py

--- flask-on-docker/services/web/manage.py
+++ flask-on-docker/services/web/manage.py
@@ -1,10 +1,17 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db
 
 
 cli = FlaskGroup(app)
 
+
+@cli.command("create_db")
+def create_db():
+    #db.drop_all()
+    #db.create_all()
+    #db.session.commit()
+    return 0
 
 if __name__ == "__main__":
     cli()
