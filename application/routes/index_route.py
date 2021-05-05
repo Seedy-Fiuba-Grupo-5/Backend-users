a40206d49d736bf786e04d220440faa9ceaca91f
Yuquimura <brianzambellitello@gmail.com>
2021-05-05 19:30:12 -0300

    Documentacion del archivo

--- application/routes/index_route.py
+++ application/routes/index_route.py
@@ -2,6 +2,7 @@ from flask import render_template
 from application import app
 
 
+# Funcion que devuelve el template asociado al inicio de la pagina
 @app.route("/") 
 def hello_world():
-    return render_template('pantalla_inicio.html')
+    return render_template('start_screen.html')
