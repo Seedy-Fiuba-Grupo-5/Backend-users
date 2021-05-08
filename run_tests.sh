58f5b970dca2230d860f796cd54ed844fe79ac2d
Yuquimura <brianzambellitello@gmail.com>
2021-05-08 13:55:25 -0300

    Agregado: Agrego flag por si queda algun orphan

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "[RUN_TESTS] Starting services ..."
-docker-compose up -d --build
+docker-compose up -d --build --remove-orphans
 echo "[RUN_TESTS] Running tests ..."
 docker-compose exec service_users_web pytest "backend_users/tests" -p no:warnings --cov="backend_users"
 echo "[RUN_TESTS] Running flake8 inside all folders"
