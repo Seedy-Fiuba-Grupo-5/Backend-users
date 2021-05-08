f9c697c9eeda61d560ea181c4ba7543d429dc96c
Yuquimura <brianzambellitello@gmail.com>
2021-05-08 20:01:21 -0300

    Add: Agrego blueprint al users_v1_api.py. (No funciona aun)

--- backend_users/api/users_v1_api.py
+++ backend_users/api/users_v1_api.py
@@ -1,11 +1,13 @@
 from flask import render_template
-from application import app
-from application.tables.users_table import User
+from backend_users.tables.users_table import User
+from flask import Blueprint
+
+users_v1_api = Blueprint("users_v1_api", __name__)
 
 
 # Metodo para imprimir la tabla users de la base de datos
 # PRE: Es necesario que la base de datos db exista
-@app.route("/v1/users")
+@users_v1_api.route('/v1/users')
 def imprimir_base_de_datos():
     db2 = User.query.all()
     return render_template('users.html',
