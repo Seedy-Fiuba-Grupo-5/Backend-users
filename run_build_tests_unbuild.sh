38afa9c4e3d2fffa3d3859858b0a7cdd6c5b299c
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-09 17:55:10 -0300

    bug: Parece que hay que esperar un tiempo entre que se levantan los servicios y se ejecutan los tests

--- run_build_tests_unbuild.sh
+++ run_build_tests_unbuild.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+echo "[RUN_TESTS] Starting services ..."
+docker-compose up -d --build --remove-orphans
 echo "[RUN_TESTS] Running tests ..."
 docker-compose exec service_users_web pytest "backend_users/tests" -p no:warnings --cov="backend_users"
 echo "[RUN_TESTS] Running flake8 inside all folders"
 docker-compose exec service_users_web flake8 backend_users
+echo "[RUN_TESTS] Turning down services ..."
+docker-compose down -v
