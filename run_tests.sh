1a8e3fef8fd85e1b8417bb4719bf2542e33ae3e9
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-10 14:36:16 -0300

    feat: Agrego codecov a requirements.txt

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ echo "[RUN_TESTS] waiting for postgres to start ..."
 docker-compose exec service_users_web bash -c 'while ! nc -z service_users_db 5432; do sleep 1; done;'
 
 echo "[RUN_TESTS] Running tests ..."
-docker-compose exec service_users_web pytest "backend_users/tests" -p no:warnings --cov="backend_users"
+docker-compose exec service_users_web pytest "backend_users/tests" --cov="backend_users"
 
 echo "[RUN_TESTS] Running flake8 inside all folders"
 docker-compose exec service_users_web flake8 backend_users
