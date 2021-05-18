1efbed9333744e8f37447312e310e456f63a45c5
Yuquimura <brianzambellitello@gmail.com>
2021-05-18 14:51:50 -0300

    Fix: Refactor de nombres para mayor claridad

--- backend_users/prod/api/users_list_api.py
+++ backend_users/prod/api/users_list_api.py
@@ -2,15 +2,15 @@ from flask import Blueprint  # , request
 from flask_restful import Api, Resource
 from prod.db_models.user_db_model import UserDBModel
 
-users_api = Blueprint("users_api", __name__)
-api = Api(users_api)
+users_list_api = Blueprint("users_list_api", __name__)
+api = Api(users_list_api)
 
 
-class UsersResource(Resource):
+class UsersListResource(Resource):
     def get(self):
         response_object =\
             [user.serialize() for user in UserDBModel.query.all()]
         return response_object, 200
 
 
-api.add_resource(UsersResource, "/users")
+api.add_resource(UsersListResource, "/users")
