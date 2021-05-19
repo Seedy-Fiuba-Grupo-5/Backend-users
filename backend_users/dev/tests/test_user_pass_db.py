773ad70798c7e0ff47fc0f0702ed2881337adb7e
Yuquimura <brianzambellitello@gmail.com>
2021-05-19 13:05:49 -0300

    New: Nuevo chequeo dentro del archivo

--- backend_users/dev/tests/test_user_pass_db.py
+++ backend_users/dev/tests/test_user_pass_db.py
@@ -19,3 +19,7 @@ def test_devuelve_true_cuando_la_relacion_usuario_pass_es_correcta(test_app,
     assert UserDBModel.comprobar_relacion_usuario_pass(
         "fdimaria@fi.uba.ar",
         "hola") is True
+    assert UserDBModel.comprobar_relacion_usuario_pass(
+        "fdimaria@fi.uba.ar",
+        "hola2") is False
+
