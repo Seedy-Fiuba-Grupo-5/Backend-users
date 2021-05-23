68f977bf6c86b2a455158e2150a552e78abef7ba
Yuquimura <brianzambellitello@gmail.com>
2021-05-23 16:49:13 -0300

    Refactor: Unificacion de metodos comprobar_relacion_usuario_pass y get_id para mejorar performance. Fix: Modificacion de tests para reflejar nuevo comportamiento

--- backend_users/dev/tests/test_user_pass_db.py
+++ backend_users/dev/tests/test_user_pass_db.py
@@ -13,14 +13,12 @@ def test_devuelve_true_cuando_la_relacion_usuario_pass_es_correcta(test_app,
                             email="fdimaria@fi.uba.ar",
                             password="hola"))
     session.commit()
-    assert UserDBModel.comprobar_relacion_usuario_pass(
+    assert UserDBModel.get_id(
         "bzambelli@fi.uba.ar",
-        "hola") is False
-    assert UserDBModel.comprobar_relacion_usuario_pass(
+        "hola") == -1
+    assert UserDBModel.get_id(
         "fdimaria@fi.uba.ar",
-        "hola") is True
-    assert UserDBModel.comprobar_relacion_usuario_pass(
+        "hola") is 1
+    assert UserDBModel.get_id(
         "fdimaria@fi.uba.ar",
-        "hola2") is False
-    assert UserDBModel.get_id("fdimaria@fi.uba.ar",
-                              "hola") == 1
+        "hola2") is -1
