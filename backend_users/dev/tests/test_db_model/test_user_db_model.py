539e3cd725f5ea51dfcbce5d7f2b9a363b92ebdc
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-24 14:31:18 -0300

    Doc: Documento test_user_db_model

--- backend_users/dev/tests/test_db_model/test_user_db_model.py
+++ backend_users/dev/tests/test_db_model/test_user_db_model.py
@@ -1,14 +1,16 @@
 from prod.db_models.user_db_model import UserDBModel
+from dev.aux_test import recreate_db
 
 
-def test_get_minus_one_when_register_user_twice(
-        test_app,
-        test_database
-):
-    session = test_database.session
-    session.remove()
-    test_database.drop_all()
-    test_database.create_all()
+def test_userdbmodel_add_user_dos_veces_al_mismo_usuario_devuelve_menos_1(
+    test_app,
+    test_database):
+    """
+    Dada una base de datos vacia
+    Invocar UserDBModel.add_user(<usuario 1>) => 1
+    Invocar otra vez UserDBModel.add_user(<usuario 1>) => -1
+    """
+    session = recreate_db(test_database)
     assert 1 == UserDBModel.add_user(name="Franco", lastname="Di Maria",
                                      email="fdimaria@fi.uba.ar",
                                      password="hola")
@@ -17,13 +19,22 @@ def test_get_minus_one_when_register_user_twice(
                                       password="hola")
 
 
-def test_get_id_only_when_pass_and_user_is_correct(test_app,
-                                                   test_database
-                                                   ):
-    session = test_database.session
-    session.remove()
-    test_database.drop_all()
-    test_database.create_all()
+def test_get_id_only_when_pass_and_user_is_correct(
+    test_app,
+    test_database):
+    """
+    Dada una base de datos
+    Y un usuario registrado:
+        name = "Franco Martin"
+        lastName = "Di Maria"
+        email = "fdimaria@fi.uba.ar"
+        password = "hola" 
+        Id = 1
+    Invocar UserDBModel.get_id("bzambelli@fi.uba.ar", "hola") => -1
+    Invocar UserDBModel.get_id("fdimaria@fi.uba.ar", "hola")  =>  1
+    Invocar UserDBModel.get_id("fdimaria@fi.uba.ar", "hola2") => -1
+    """
+    session = recreate_db(test_database)
     session.add(UserDBModel(name="Franco Martin",
                             lastname="Di Maria",
                             email="fdimaria@fi.uba.ar",
