23909e5174cf5611f4a5b541ae288ad40987110a
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-23 23:05:57 -0300

    Doc: Documento pruebas user_login_api

--- backend_users/dev/tests/test_users_login_api.py
+++ backend_users/dev/tests/test_users_login_api.py
@@ -4,6 +4,21 @@ from dev.aux_test import recreate_db
 
 
 def test_dado_email_fdimaria_registrado_con_id_1_y_palabra_de_pase_tomate_cuando_POST_a_url_users_barra_login_el_email_y_la_palabra_de_pase_obtengo_el_email_y_el_id_anterior(test_app, test_database):
+    """
+    Dado una base de datos con un usuario registrado:
+        nombre = "Franco Martin"
+        apellido = "Di Maria"
+        email = "fdimaria@fi.uba.ar"
+        password = "tomate"
+    Y una peticion:
+        email = "fdimaria@fi.uba.ar"
+        password = "tomate"
+    Cuando POST "/users/login"
+    Entonces obtengo status 200:
+    Y obtengo cuerpo:
+        email == "fdimaria@fi.uba.ar"
+        id == 1
+    """
     session = recreate_db(test_database)
     session.add(UserDBModel(name="Franco Martin",
                             lastname="Di Maria",
@@ -22,11 +37,27 @@ def test_dado_email_fdimaria_registrado_con_id_1_y_palabra_de_pase_tomate_cuando
     )
     login_info = json.loads(response.data.decode())
     assert response.status_code == 200
-    assert login_info["id"] == 1
     assert login_info["email"] == "fdimaria@fi.uba.ar"
+    assert login_info["id"] == 1
 
 
-def test_dado_email_fdimaria_registrado_y_palabra_de_pase_tomate_cuando_POST_a_url_users_barra_login_el_email_y_la_palabra_de_pase_manzana_obtengo_un_error_204(test_app, test_database):
+def test_dado_email_fdimaria_registrado_y_password_tomate_cuando_POST_a_url_users_barra_login_el_email_y_la_palabra_de_pase_manzana_obtengo_un_error_401(
+    test_app, 
+    test_database):
+    """
+    Dado una base de datos con un usuario registrado:
+        nombre = "Franco Martin"
+        apellido = "Di Maria"
+        email = "fdimaria@fi.uba.ar"
+        password = "tomate"
+    Y una peticion:
+        email = "fdimaria@fi.uba.ar"
+        password = "manzana"
+    Cuando POST "/users/login"
+    Entonces obtengo status 401
+    Y obtengo cuerpo:
+        'Email or password incorrect'
+    """
     session = recreate_db(test_database)
     session.add(UserDBModel(name="Franco Martin",
                             lastname="Di Maria",
@@ -48,7 +79,22 @@ def test_dado_email_fdimaria_registrado_y_palabra_de_pase_tomate_cuando_POST_a_u
     assert error == 'Email or password incorrect'
 
 
-def test_dado_email_fdimaria_registrado_y_palabra_de_pase_tomate_cuando_POST_a_url_users_barra_login_email_fdimaria_obtengo_un_error(test_app, test_database):
+def test_dado_email_fdimaria_registrado_y_palabra_de_pase_tomate_cuando_POST_a_url_users_barra_login_email_fdimaria_obtengo_un_error(
+    test_app, 
+    test_database):
+    """
+    Dado una base de datos con un usuario registrado:
+        nombre = "Franco Martin"
+        apellido = "Di Maria"
+        email = "fdimaria@fi.uba.ar"
+        password = "tomate"
+    Y una peticion:
+        email = "fdimaria@fi.uba.ar"
+    Cuando POST "/users/login"
+    Entonces obtengo status 400
+    Y obtengo cuerpo:
+        'Missing arguments'
+    """
     session = recreate_db(test_database)
     session.add(UserDBModel(name="Franco Martin",
                             lastname="Di Maria",
