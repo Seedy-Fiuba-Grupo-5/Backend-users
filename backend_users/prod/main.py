dd88593ad3fd65ae1867ab0b0896d0cfa5784ca1
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-05-26 11:06:36 -0300

    Feat: Agrego CORS a produccion Refactor: main.py en lugar de manage.py Delete: comandos de manage.py

---
+++ backend_users/prod/main.py
@@ -0,0 +1,8 @@
+from flask_cors import CORS
+from prod import create_app, db
+
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+    CORS(app)
