e7ca7ef4cc045f09e5e24f98383e162c3a46a3af
diegobalestieri <diego.balestieri1998@gmail.com>
2021-05-31 16:30:50 -0300

    agrego post de proyecto para usuario

--- backend_users/prod/api/users_projects_list_api.py
+++ backend_users/prod/api/users_projects_list_api.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Api, Resource
+from prod import db
 from prod.db_models.user_db_model import UserProjectDBModel
 
 users_projects_list_api = Blueprint("users_projects_list_api", __name__)
@@ -13,6 +14,16 @@ class UsersProjectsListResource(Resource):
         response_object = \
             [user_project.serialize() for user_project in projects_query.all()]
         return response_object, 200
+
+    def post(self, user_id):
+        data = request.get_json()
+        project_id = data['project_id']
+        user_model = UserProjectDBModel(user_id=user_id, project_id=project_id)
+        db.session.add(user_model)
+        db.session.commit()
+        db.session.refresh(user_model)
+        response_object = user_model.serialize()
+        return response_object, 201
 
 
 api.add_resource(UsersProjectsListResource, "/users/<user_id>/projects")
