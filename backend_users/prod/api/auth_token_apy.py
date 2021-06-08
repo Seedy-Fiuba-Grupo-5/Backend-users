6209726370deb2ced8606457451e1c75bf1cbbcf
Yuquimura <brianzambellitello@gmail.com>
2021-06-08 17:15:24 -0300

    Fix: Modificaciones para la integracion del nuevo endpoint

--- backend_users/prod/api/auth_token_apy.py
+++ backend_users/prod/api/auth_token_apy.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, request
 from flask_restful import Api, Resource
+from flask_restx import Namespace, Resource, fields
 from prod.db_models.user_db_model import UserDBModel
 
 REGISTER_FIELDS = ["token"]
+ns = Namespace(
+    'users/auth',
+    description='One user authentication related operation'
+)
 
-auth_api = Blueprint("auth_api", __name__)
-api = Api(auth_api)
 
-
+@ns.route('')
+@ns.param('user_id', 'The user identifier')
 class AuthenticationResource(Resource):
     def get(self):
         data = request.get_json()
